06b1b7d22dff6a036ab3b25663a2fa3ddda9e932
Mike Lang <mikelang3000@gmail.com>
2013-11-26 21:18:36 +1100

    Add generic html wrapper

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from room import Room
 app = Flask(__name__)
 
 rooms = {}
+html_template = "<html><body>{}</body></html>"
 
 DEBUG = True
 STATIC_HOMEPAGE = """
@@ -23,13 +24,13 @@ def homepage():
 @app.route('/<name>', methods=['GET'])
 def displayroom(name):
 	"""Create room if doesn't exist. Return room contents."""
-	return getroom(name).render()
+	return html_template.format(getroom(name).render())
 
 @app.route('/<name>/roll', methods=['POST'])
 def roll(name):
 	n, sides, bonus = 1, 6, 0 # TODO get args from user
 	roll = getroom(name).roll(n, sides, bonus)
-	return roll.render()
+	return html_template.format(roll.render())
 
 
 if __name__=='__main__':
